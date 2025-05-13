7de072abb8433287789e40e3352917998aa3d3d4
SebasOver <johansebastiantarazonadiaz@gmail.com>
2025-05-13 07:17:55 -0500

    Finalizacion proyecto

--- agenda/views.py
+++ agenda/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from django.db.utils import IntegrityError
-
+from .forms import TaskForm
+from .models import Task
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -24,12 +27,95 @@ def signup(request):
             except IntegrityError:
                 return render(request, 'signup.html',{
                     'form': UserCreationForm,
-                    "error": 'Contraseñas no coinciden'
+                    "error": 'Usuario ya existe'
                 })
         return render(request, 'signup.html',{
                     'form': UserCreationForm,
                     "error": 'Contraseñas no coinciden'
         })
-    
+@login_required
 def tasks(request):
- return render (request, 'tasks.html')
+    tasks = Task.objects.filter(user = request.user,datecompleted__isnull=True)
+    return render (request, 'tasks.html', {
+        'tasks': tasks
+    })
+@login_required
+def tasks_completed(request):
+    tasks = Task.objects.filter(user = request.user,datecompleted__isnull=False).order_by('-datecompleted')
+    return render (request, 'tasks.html', {
+        'tasks': tasks
+    })
+@login_required
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'create_task.html', {
+            'form': TaskForm
+        })
+    else:
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'create_task.html', {
+                'form': TaskForm,
+                'error': 'Error al crear tarea'
+    })
+@login_required
+def task_detail(request, task_id):
+    if request.method == 'GET':
+        task = get_object_or_404(Task,pk=task_id,)
+        form = TaskForm(instance=task)
+        return render(request, 'task_detail.html', {
+            'task': task,
+            'form': form
+        })
+    else:
+        try:
+            task = get_object_or_404(Task,pk=task_id, user=request.user)
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'task_detail.html', {
+            'task': task,
+            'form': form,
+            'error': 'Error al editar tarea'
+        })
+@login_required
+def complete_task(request, task_id):
+    task = get_object_or_404(Task,pk = task_id, user=request.user)
+    if request.method == 'POST':
+        task.datecompleted = timezone.now()
+        task.save()
+        return redirect('tasks')
+@login_required
+def delete_task(request, task_id):
+    task = get_object_or_404(Task,pk = task_id, user=request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('tasks')
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html',{
+            'form': AuthenticationForm
+    })
+    else:
+        user = authenticate(
+            request, username = request.POST['username'], password=request.POST
+            ['password'])
+        if user is None:
+            return render(request, 'signin.html',{
+                'form': AuthenticationForm,
+                'error' : 'Usuario o contraseña incorrectos'
+            })
+        else:
+            login(request, user)
+            return redirect('tasks')
